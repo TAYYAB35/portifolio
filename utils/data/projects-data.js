eccb2a87e07851e5d9625f99645de3ae61681edc
MTAYABAIG <mtayyabbaig26@gmail.com>
2025-06-30 21:25:14 +0500

    new proj

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -1,6 +1,24 @@
 export const projectsData = [
     {
+        id: 0,
+        name: 'Top Thoughts',
+        description: "Me and my team built an AI-powered BALKE Digital Marketing Solution.I have intergrate the API and build the UI using tailwindcss and scss in Angular.",
+        tools: ['React', 'Tailwindcss', 'SCSS', 'Text Editor', 'Express', 'Nodejs', 'MongoDB'],
+        role: 'Full Stack and UI Developer',
+        code: '',
+        demo: 'https://topthoughts.vercel.app/',
+    },
+    {
         id: 1,
+        name: 'BIOGROW+',
+        description: "Built an organic product website in React to showcase three core solutions (Hormonal, Pest, Nutrition) using a responsive carousel. Styled with Tailwind CSS and SCSS to ensure a clean, modern UI for promoting eco-friendly, herbal-based plant care",
+        tools: ['Next', 'Tailwindcss', 'SCSS','Transition', 'SwiperJS', 'ScrollReveal','Dark Mode'],
+        role: 'Frontend and UI Developer',
+        code: '',
+        demo: 'https://herbal-shield.vercel.app/',
+    },
+    {
+        id: 2,
         name: 'BALKE',
         description: "Me and my team built an AI-powered BALKE Digital Marketing Solution.I have intergrate the API and build the UI using tailwindcss and scss in Angular.",
         tools: ['Angular', 'Tailwindcss', 'SCSS', 'AOS', 'Recaptcha'],
@@ -9,7 +27,7 @@ export const projectsData = [
         demo: 'https://balke.tech/',
     },
     {
-        id: 2,
+        id: 3,
         name: 'PROSHOP',
         description: 'I have designed and developed a full-stack web app for ECOMMERCE.I created the UI using React, Javascript,TailwindCSS.The app accept multiple payment and currencies. I developed the API using React,Nodejs,Express, and MongoDB.',
         tools: ['React', 'Node', 'Express', 'MongoDB', 'Bootstrap', 'Redux'],
@@ -18,7 +36,7 @@ export const projectsData = [
         demo: '',
     },
     {
-        id: 3,
+        id: 4,
         name: 'DigiContest',
         description: 'I have converted the project in relaity using following Techs',
         tools: ['Angular', 'Bootstrap', 'SCSS', 'ChartJs', 'Google Charts', 'NgZoro'],
@@ -27,7 +45,7 @@ export const projectsData = [
         demo: 'https://streamable.com/mpgj5i',
     },
     {
-        id: 4,
+        id: 5,
         name: 'Luqta Website',
         description: "Created the portiloio website for digicontesst",
         tools: ['Angular', 'Bootstrap', 'SCSS', 'NgZoro'],
@@ -36,21 +54,12 @@ export const projectsData = [
         role: 'UI Developer',
     },
     {
-        id: 5,
+        id: 6,
         name: "ELTAO ECOM",
         description: "Designed and developed the complete UI for an e-commerce platform, ensuring a seamless and user-friendly experience.",
         tools: ["Angular", "Bootstrap", "SCSS", "NgZoro"],
         code: "",
         demo: "https://streamable.com/tnprof",
-        role: "UI Developer"
-    },
-    {
-        id: 6,
-        name: "TAILWIND ECOM",
-        description: "Developed a fully responsive e-commerce UI using modern design principles, integrating animations and smooth interactions.",
-        tools: ["Angular", "TailwindCSS", "SCSS", "NgZoro", "OwlCarousel", "AOS"],
-        code: "",
-        demo: "https://logoipsum-sigma.vercel.app/",
         role: "UI Developer"
     },
     {
@@ -75,7 +84,7 @@ export const projectsData = [
         id: 9,
         name: "PLANT",
         description: "Plants make a positive impact on your environment",
-        tools: ["HTML",'JS',"TailwindCSS", "CSS", "ScrollReveal", "SwiperJS"],
+        tools: ["HTML", 'JS', "TailwindCSS", "CSS", "ScrollReveal", "SwiperJS"],
         code: "",
         demo: "https://plant-beryl.vercel.app/",
         role: "Front-end Developer"
@@ -84,7 +93,7 @@ export const projectsData = [
         id: 10,
         name: "SPACE",
         description: "Created and destroyed the space website using following tools",
-        tools: ["HTML",'JS',"TailwindCSS", "CSS", "ScrollReveal", "SwiperJS"],
+        tools: ["HTML", 'JS', "TailwindCSS", "CSS", "ScrollReveal", "SwiperJS"],
         code: "",
         demo: "https://space-eight-chi.vercel.app/",
         role: "Front-end Developer"
@@ -93,7 +102,7 @@ export const projectsData = [
         id: 11,
         name: "CACTUS",
         description: "Created and destroyed the space website using following tools",
-        tools: ["HTML",'JS',"TailwindCSS", "CSS", "ScrollReveal", "SwiperJS"],
+        tools: ["HTML", 'JS', "TailwindCSS", "CSS", "ScrollReveal", "SwiperJS"],
         code: "",
         demo: "https://mtayabaig.github.io/cactus/",
         role: "Front-end Developer"
@@ -102,10 +111,18 @@ export const projectsData = [
         id: 12,
         name: "TATADEV",
         description: "Created and destroyed the Portifolio website of tata Software House",
-        tools: ["Angular",'JS',"Bootstrap", "CSS", "AOS", "OwlCarosule"],
+        tools: ["Angular", 'JS', "Bootstrap", "CSS", "AOS", "OwlCarosule"],
         code: "",
         demo: "https://streamable.com/co4ijx",
         role: "Front-end Developer"
+    }, {
+        id: 13,
+        name: "TAILWIND ECOM",
+        description: "Developed a fully responsive e-commerce UI using modern design principles, integrating animations and smooth interactions.",
+        tools: ["Angular", "TailwindCSS", "SCSS", "NgZoro", "OwlCarousel", "AOS"],
+        code: "",
+        demo: "https://logoipsum-sigma.vercel.app/",
+        role: "UI Developer"
     },
 
 ];
